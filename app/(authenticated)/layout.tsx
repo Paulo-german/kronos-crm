0aa49c95ce76c93ac94f96d1bf467cb9ba6c4a1e
Paulo Germano <paulo.german777@gmail.com>
2026-06-03 21:50:35 -0300

    refactor(layout): remove legacy sidebar components and SidebarProvider from authenticated layout

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -1,23 +1,11 @@
-import { cookies } from 'next/headers'
 import { TooltipProvider } from '@/_components/ui/tooltip'
-import { SidebarProvider } from '@/_providers/sidebar-provider'
 
 interface AuthenticatedLayoutProps {
   children: React.ReactNode
 }
 
-const AuthenticatedLayout = async ({ children }: AuthenticatedLayoutProps) => {
-  const cookieStore = await cookies()
-  const sidebarCookie = cookieStore.get('kronos-sidebar-collapsed')
-  const defaultCollapsed = sidebarCookie?.value === 'true'
-
-  return (
-    <SidebarProvider defaultCollapsed={defaultCollapsed}>
-      <TooltipProvider>
-        {children}
-      </TooltipProvider>
-    </SidebarProvider>
-  )
+const AuthenticatedLayout = ({ children }: AuthenticatedLayoutProps) => {
+  return <TooltipProvider>{children}</TooltipProvider>
 }
 
 export default AuthenticatedLayout
